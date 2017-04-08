fda2e9362103d17d6c441a3ebe9320ecebf2584f
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-04-08 16:55:09 +0000

    Added user authentication to create new place, created new migration for user id and linked user id to new place and added email

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @places = Place.all
     @places = Place.paginate(:per_page => 5, :page => params[:page]).order('created_at DESC')
@@ -13,7 +14,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
