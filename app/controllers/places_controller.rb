f2966e41cd600813576ea29207ad10cdf8e936ea
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-04-09 22:12:01 +0000

    Created code to only allow users who created a place to be able to edit it or destroy it.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     @places = Place.all
     @places = Place.paginate(:per_page => 5, :page => params[:page]).order('created_at DESC')
@@ -24,10 +24,19 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
-  end
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+   end 
 
   def update
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end 
+    
     @place.update_attributes(place_params)
     redirect_to root_path
   end
