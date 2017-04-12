dbbbd45312263500c9ff4739f42a2b2e448ef64e
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-04-12 01:07:13 +0000

    Set up Google Map integration, added geocoder gem and figaro gem to store api.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   validates :name, :address, :description, presence: true
   validates :description, :address, length: {minimum: 3 }
 
