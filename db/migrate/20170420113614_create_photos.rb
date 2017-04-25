4c346eb2511994f1b131d5256689ef8fd1fe161f
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-04-25 01:25:01 +0000

    Created modal for photos and put photo form in modal.

--- db/migrate/20170420113614_create_photos.rb
+++ db/migrate/20170420113614_create_photos.rb
@@ -1,7 +1,6 @@
 class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
     create_table :photos do |t|
-      t.file :photo
       t.text :caption
       t.integer :user_id
       t.integer :place_id
