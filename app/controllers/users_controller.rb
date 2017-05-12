d876e9e81a516dcc6ead746c77f54da0e06ea304
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-05-12 01:08:04 +0000

    Added username to user and set up params in controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit 
+    if !current_user || current_user.id != params[:id].to_i
+      render status: :forbidden 
+    end
+  end 
+
+  def update
+    if !current_user || current_user.id != params[:id].to_i
+      render status: :forbidden 
+    else 
+      current_user.update(user_params)
+      redirect_to user_path(current_user)
+    end
+  end   
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
 end
