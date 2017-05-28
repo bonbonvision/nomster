66e40ff11b7c450d5e7014ec687ac74f9f1ea026
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-05-28 01:40:48 +0000

    Added slick folder with fonts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,14 +19,33 @@
 // =require tether
 
 
-function initSlider(){
-    $('.regular').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        autoplay: true,
-        prevArrow: '<div class="slick-prev"><i class="fa fa-chevron-left"></i></div>',
-        nextArrow: '<div class="slick-next"><i class="fa fa-chevron-right"></i></div>'
+$(document).ready(function() {
+  $('.responsive').slick({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
     });
-}
+  });
