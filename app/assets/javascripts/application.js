695e1ee39c7ba74afb870b7fa2939f19d0b876ca
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-05-31 11:19:21 +0000

    Edited CSS of slickslider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,32 +20,36 @@
 
 
 $(document).ready(function() {
-  $('.responsive').slick({
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        }, {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }]
+  $('.places-slider').slick({
+    arrows: true,
+    dots: true,
+    infinite: false,
+    // variableWidth: true,
+    centerMode: true,
+    centerPadding: '40px',
+    
+    // slidesToShow: 4,
+        // slidesToScroll: 4,
+        // responsive: [{
+        //     breakpoint: 1024,
+        //     settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 3,
+        //         infinite: true,
+        //         dots: true
+        //     }
+        // }, {
+        //     breakpoint: 600,
+        //     settings: {
+        //         slidesToShow: 2,
+        //         slidesToScroll: 2
+        //     }
+        // }, {
+        //     breakpoint: 480,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1
+        //     }
+        // }]
     });
   });
